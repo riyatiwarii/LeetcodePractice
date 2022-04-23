a248d906a7a00c5d53d6f7b5adac1be4b63e3135
Riya Tiwari <riyatiwari727@gmail.com>
2022-04-23 22:06:11 +0530

    3

--- Strings(EASY)/String.2.py
+++ Strings(EASY)/String.2.py
@@ -1,3 +1,18 @@
+'''Write a function to find the longest common prefix string amongst an array of strings.
+If there is no common prefix, return an empty string "".'''
+
+#My preferred way
+def longestCommonPrefix(self, strs):
+    if len(strs) == 0:
+        return ""
+    s1, s2 = min(strs), max(strs)
+    i = 0
+    while i < len(s1) and i < len(s2) and s1[i] == s2[i]:
+        i += 1
+    return s1[:i]
+print(commonprefix(['far', 'farm', 'farming']))
+
+# Another way
 def longestCommonPrefix(strs):
     l = len(strs[0])
     res = ""
@@ -10,4 +25,4 @@ def longestCommonPrefix(strs):
                 return res
         res += strs[0][i]
     return res
-print(longestCommonPrefix(['flower','flow','flight']))
+print(longestCommonPrefix(['winner','windown','wink']))
---
+++ Strings(EASY)/String.3.py
@@ -0,0 +1,7 @@
+#Parentheses problem DUE
+'''Length of Last Word'''
+str = "   fly me   to   the moon  "
+# w = str.split()
+# print(w)
+print(len(str.split()[-1]))
+
