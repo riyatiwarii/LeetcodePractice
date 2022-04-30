a56a87a5c8bccaf53507e2aaebdc690f895d4820
Riya Tiwari <riyatiwari727@gmail.com>
2022-04-30 19:10:54 +0530

    12

---
+++ Strings(EASY)/String12.py
@@ -0,0 +1,20 @@
+def reverseVowels(s):
+    vowels = set('aeiouAEIOU')
+    s = list(s)
+    left = 0
+    right = len(s) - 1
+    while left <= right:
+        if s[left] in vowels and s[right] in vowels:
+            s[left], s[right] = s[right], s[left]
+        elif s[left] not in vowels:
+            left+= 1
+            continue
+        elif s[right] not in vowels:
+            right-= 1
+            continue
+        left += 1
+        right -= 1
+    return ''.join(s)
+print(reverseVowels("Riya"))
+
+
