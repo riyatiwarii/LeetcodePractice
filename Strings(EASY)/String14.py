819f4001533b3d5d9f040846c0c47f556b03e379
Riya Tiwari <riyatiwari727@gmail.com>
2022-05-01 23:48:54 +0530

    16

--- Strings(EASY)/String14.py
+++ Strings(EASY)/String14.py
@@ -1,12 +1,12 @@
-def firstUniqChar(str):
-    freq = {}
-    for char in range(0, len(str)):
-        if str[char] not in freq:
-            freq[str[char]] = True
+def firstUniqChar(s):
+    frequecy = {}
+    for i in range(len(s)):
+        if s[i] not in frequecy:
+            frequecy[s[i]] = 1
         else:
-            freq[str[char]] = False
-    for char in range(0, len(str)):
-        if freq[str[char]]:
-            return char
+            frequecy[s[i]] = frequecy[s[i]] + 1
+    for i in range(len(s)):
+        if frequecy[s[i]]==1:
+            return i
     return -1
 print(firstUniqChar("test"))
---
+++ Strings(EASY)/String16.py
@@ -0,0 +1,9 @@
+'''isSubsequence'''
+def isSubsequence(s,t):
+    i,j = 0, 0
+    while i<len(s) and j<len(t):
+        if s[i] == t[j]:
+            i += 1
+        j += 1
+    return True if i == len(s) else False
+print(isSubsequence("abc","abcde"))
