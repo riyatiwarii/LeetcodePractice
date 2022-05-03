716d7f6ceee1a68181e3600ef9212d804aec7857
Riya Tiwari <riyatiwari727@gmail.com>
2022-05-03 14:49:51 +0530

    17

---
+++ Strings(EASY)/String17.py
@@ -0,0 +1,13 @@
+'''Length of the Longest Palindrome'''
+def longestPalindrome(s):
+    ss = set()
+    for letter in s:
+        if letter not in ss:
+            ss.add(letter)
+        else:
+            ss.remove(letter)
+    if len(ss) != 0:
+        return len(s) - len(ss) + 1
+    else:
+        return len(s)
+print(longestPalindrome("abccccdd"))
