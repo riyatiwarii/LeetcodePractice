978bd9cb6fb85b45e54b59dac549e444f6b2cc45
Riya Tiwari <riyatiwari727@gmail.com>
2022-05-06 01:07:07 +0530

    28

---
+++ Strings(EASY)/String28.py
@@ -0,0 +1,7 @@
+def sortSentence(s):
+    slist = s.split()
+    sdic = {}
+    for i in slist:
+        sdic[i[-1]] = i[:-1]
+    return " ".join(sdic[j] for j in sorted(sdic))
+print(sortSentence("is2 sentence4 This1 a3"))
