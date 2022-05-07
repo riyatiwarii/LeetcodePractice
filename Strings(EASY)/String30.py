4160dd1bc1d7c3b35ee77884b51eb8225248df43
Riya Tiwari <riyatiwari727@gmail.com>
2022-05-07 17:46:26 +0530

    30

---
+++ Strings(EASY)/String30.py
@@ -0,0 +1,12 @@
+def maxDepth(s):
+    count = 0
+    maxnum = 0
+    for i in s:
+        if i == "(":
+            count += 1
+            if maxnum < count:
+                maxnum = count
+        if i == ")":
+            count -= 1
+    return maxnum
+print(maxDepth("(1)+((2))+(((3)))"))
