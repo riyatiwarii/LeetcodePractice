65f835846aa06f528372d2dd1ac967997d3ce2fd
Riya Tiwari <riyatiwari727@gmail.com>
2022-05-12 10:25:00 +0530

    47

--- Strings(EASY)/String47.py
+++ Strings(EASY)/String47.py
@@ -1,3 +1,4 @@
+'''Determine color of a chessboard square.'''
 def squareIsWhite(coordinates):
     return True if ((ord(coordinates[0])) + int(coordinates[1])) % 2 else False
 print(squareIsWhite("d2"))
