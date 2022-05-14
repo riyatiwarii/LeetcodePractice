532aa2ce5ef78ceb4142f255673627bb26d64c66
Riya Tiwari <riyatiwari727@gmail.com>
2022-05-14 21:51:06 +0530

    54

---
+++ Strings(EASY)/String54.py
@@ -0,0 +1,17 @@
+def kthDistinct(arr, k):
+    from collections import Counter
+    l=[]
+    c=Counter(arr)
+    for i in arr:
+        if c[i] == 1:
+            l.append(i)
+    if k > len(l):
+        return ""
+    else:
+        return l[k-1]
+print(kthDistinct(["d", "b", "c", "b", "c", "a"], 2))
+print(kthDistinct(["aaa","aa","a"], 2))
+print(kthDistinct(["a","b","a"], 2))
+
+
+
