a4b8915974107c62dfe52c5b52e9572d723d0a15
Riya Tiwari <riyatiwari727@gmail.com>
2022-08-16 22:59:50 +0530

    2

---
+++ LinkedList/Easy_2.py
@@ -0,0 +1,28 @@
+# Definition for singly-linked list.
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+        '''Approach one'''
+class Solution:
+    def middleNode(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        count = 0
+        temp = head
+        while temp.next is not None:
+            temp = temp.next
+            count += 1
+        if (count % 2) == 1:
+            mid = count // 2
+        else:
+            mid = count+1 // 2
+        for i in range(mid):
+            head = head.next
+        return head
+        '''Appproach second'''
+class Solution:
+    def middleNode(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        slow = fast = head
+        while fast and fast.next is not None:
+            slow = slow.next
+            fast = fast.next.next
+        return slow
